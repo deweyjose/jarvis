d91ed34f66e2d396925eb59abab8daa460ebfce4
Mark Faga <mjfaga@gmail.com>
2023-03-02 21:00:20 -0500

    feat: use a ref instead of change handler

--- src/App.js
+++ src/App.js
@@ -1,52 +1,56 @@
-import { useState } from 'react';
-import './App.css';
+import { useState, useRef } from "react";
+import "./App.css";
 
 function Prompt() {
-
+  const prompt = useRef(null);
   const [inputs, setInputs] = useState({
     loading: false,
-    prompt: '',
-    response: ''
+    response: "",
   });
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(values => ({ ...values, [name]: value }))
-  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setInputs(values => ({ ...values, loading: true }));
-    fetch('https://sm6bgqavpagpz4i5c4nl5ty2c40dnudl.lambda-url.us-east-1.on.aws/', {
-      method: "POST",
-      body: JSON.stringify({ prompt: inputs.prompt })
-    })
-      .then(response => response.json())
-      .then((response) => {
-        setInputs(values => ({ ...values, loading: false, response: response[0].text.trim() }))
-      }, (error) => {
-        console.log('error', error);
-        setInputs(values => ({ ...values, response: error.message }))
-      })
-  }
+    setInputs((values) => ({ ...values, loading: true }));
+    fetch(
+      "https://sm6bgqavpagpz4i5c4nl5ty2c40dnudl.lambda-url.us-east-1.on.aws/",
+      {
+        method: "POST",
+        body: JSON.stringify({ prompt: prompt.current.value }),
+      }
+    )
+      .then((response) => response.json())
+      .then(
+        (response) => {
+          setInputs((values) => ({
+            ...values,
+            loading: false,
+            response: response[0].text.trim(),
+          }));
+        },
+        (error) => {
+          console.log("error", error);
+          setInputs((values) => ({ ...values, response: error.message }));
+        }
+      );
+  };
 
   return (
-    (<div className="App-container">
-      <div className='App-greeting'>
+    <div className="App-container">
+      <div className="App-greeting">
         My name is Jarvis. <br /> What's on your mind?
       </div>
       <div className="App-form">
-        <textarea name="prompt" onChange={handleChange} value={inputs.prompt} className="App-text-area" />
-        <button type="submit" onClick={handleSubmit}>Go!</button>
+        <textarea name="prompt" ref={prompt} className="App-text-area" />
+        <button type="submit" onClick={handleSubmit}>
+          Go!
+        </button>
       </div>
       <div className="App-response-area">
         {inputs.loading && <i>loading...</i>}
         {!inputs.loading && inputs.response}
       </div>
-
-    </div >)
-  )
+    </div>
+  );
 }
 
 function App() {
