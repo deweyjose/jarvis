b44af76e655fb99331526ca14abc309e69d0b0eb
Dewey Jose <richard.e.jose@gmail.com>
2023-03-03 16:32:32 -0500

    add some sound

--- src/components/PromptExperience.js
+++ src/components/PromptExperience.js
@@ -1,18 +1,39 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./PromptExperience.css";
 
-function PromptForm({ prompt, handleSubmit }) {
+
+function PromptForm({ prompt, handleSubmit, speachRate, speachEnabled }) {
   return (
-    <form className="PromptForm">
+    <div className="PromptForm">
       <textarea name="prompt" ref={prompt} rows="5" />
-      <button type="submit" onClick={handleSubmit}>
-        Go!
-      </button>
-    </form>
+      <div className="PromptFormControls">
+        <div className="PromptFormAudioControls">
+          <div className="PromptFormAudioControl">
+            <label for="rate" className="PromptFormLabel">Speach Rate: </label>
+            <input type="range" ref={speachRate} min="0" max="2" step="0.1" defaultValue="1.5" name="rate" />
+          </div>
+          <div className="PromptFormAudioControl">
+            <label className="PromptFormLabel">Speach Enabled:</label>
+            <input type="checkbox" ref={speachEnabled} defaultChecked="true" />
+          </div>
+        </div>
+        <div className="PromptFormButton">
+          <button type="submit" onClick={handleSubmit}>
+            Go!
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
-function PromptResponse({ loading, response }) {
+function PromptResponse({ loading, speak, response, speachRate, speachEnabled }) {
+  useEffect(() => {
+    if (!loading && speachEnabled.current.checked) {
+      speak(response, speachRate.current.value);
+    }
+  });
+
   return (
     <div className="PromptResponse">
       {loading && <i>loading...</i>}
@@ -23,8 +44,25 @@ function PromptResponse({ loading, response }) {
 
 export default function PromptExperience() {
   const prompt = useRef(null);
+  const speachRate = useRef(null);
+  const speachEnabled = useRef(null);
   const [loading, setLoading] = useState(false);
   const [response, setResponse] = useState("");
+
+  const speak = (text, speed) => {
+
+    const msg = new SpeechSynthesisUtterance(text);
+    const synth = window.speechSynthesis;
+
+    if (synth.speaking) {
+      synth.cancel();
+    }
+
+    const voices = synth.getVoices();
+    msg.voice = voices[7];
+    msg.rate = speed;
+    synth.speak(msg);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -51,8 +89,8 @@ export default function PromptExperience() {
 
   return (
     <>
-      <PromptForm prompt={prompt} handleSubmit={handleSubmit} />
-      <PromptResponse loading={loading} response={response} />
+      <PromptForm prompt={prompt} handleSubmit={handleSubmit} speachRate={speachRate} speachEnabled={speachEnabled} />
+      <PromptResponse loading={loading} speak={speak} response={response} speachRate={speachRate} speachEnabled={speachEnabled} />
     </>
   );
 }
